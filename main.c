b26df38362c8d93f97cb7cb4cbd4889c38566e6d
Juma Antony <jumaantony840@gmail.com>
2023-04-19 07:18:46 +0300

    working on how to exit the shell

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ int main(void) {
 
     size_t size = 0;
 
+    ssize_t readn_chars;
+
     // Adding an infinite loop to keep the shell running until the user exits
 
     while (1) {
@@ -18,7 +20,20 @@ int main(void) {
         printf("%s", printprompt);
 
         // getline() func reads in an input from the stdin and stores it in the lineprinter variable
-        getline(&lineprinter, &size, stdin);
+        // getline(&lineprinter, &size, stdin);
+
+        readn_chars = getline(&lineprinter, &size, stdin);
+
+        // checking if EOF has been reached or if an error has occured or if the user exits the shell
+        if (readn_chars == EOF || readn_chars == -1) {
+            // // freeing the memory allocated to lineprinter by the getline() func
+            // free(lineprinter);
+            // // exiting the shell
+            // exit(0);
+
+            printf("Byeeeee!! Too bad you are leaving...\n");
+            return (-1);
+        }
 
         // printing the input from lineprinter
         printf("%s\n", lineprinter);
