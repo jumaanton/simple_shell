e525809fe56295ddbf715893cafaf44e1c630363
Albetro <albertxang@gmail.com>
2023-04-27 01:50:17 +0300

    builtins

--- shell.h
+++ shell.h
@@ -18,13 +18,13 @@ extern char **environ;
 extern __sighandler_t signal(int __sig, __sighandler_t __handler);
 
 /* handle built ins */
-int checker(char **cmd, char *buf);
+int checks(char **cmd, char *buf);
 void prompt_user(void);
 void handle_signal(int m);
 char **tokenizer(char *line);
 char *test_path(char **path, char *command);
 char *append_path(char *path, char *command);
-int handle_builtin(char **command, char *line);
+int builtin(char **command, char *line);
 void exit_cmd(char **command, char *line);
 
 void print_env(void);
