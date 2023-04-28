aacb0b3f02c6b1e3e24cc1040c3b38af42210d59
Albetro <albertxang@gmail.com>
2023-04-29 00:23:31 +0300

    shell commit

--- shell.c
+++ shell.c
@@ -1,50 +1,127 @@
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 #include "shell.h"
+#include "main.h" /* for _printf */
+#define BUFFER_SIZE 4096
+param_t *init_param(char **argv, char **env);
 /**
-* main - carries out the read, execute then print output loop
-* @ac: argument count
-* @av: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
-
-int main(int ac, char **av, char *envp[])
+ * main - entry point for simple shell
+ * @argc: argument count
+ * @argv: Null terminated argument list
+ * @env: Null terminated environment variables list
+ *
+ * Return: 0 on success
+ */
+int main(int __attribute__((unused)) argc, char **argv, char **env)
 {
-	char *line = NULL, *pathcommand = NULL, *path = NULL;
-	size_t bufsize = 0;
-	ssize_t linesize = 0;
-	char **command = NULL, **paths = NULL;
-	(void)envp, (void)av;
-	if (ac < 1)
-		return (-1);
-	signal(SIGINT, handle_signal);
+	param_t *params = NULL;
+	int cond = -2, status;
+	unsigned int i;
+	char *state = NULL;
+	size_t size = BUFFER_SIZE;
+
+	params = init_param(argv, env);
+	if (!params)
+		exit(-1);
+	signal(SIGINT, sigint_handler);
 	while (1)
 	{
-		free_buffers(command);
-		free_buffers(paths);
-		free(pathcommand);
-		prompt_user();
-		linesize = getline(&line, &bufsize, stdin);
-		if (linesize < 0)
-			break;
-		info.ln_count++;
-		if (line[linesize - 1] == '\n')
-			line[linesize - 1] = '\0';
-		command = tokenizer(line);
-		if (command == NULL || *command == NULL || **command == '\0')
-			continue;
-		if (checker(command, line))
-			continue;
-		path = find_path();
-		paths = tokenizer(path);
-		pathcommand = test_path(paths, command[0]);
-		if (!pathcommand)
-			perror(av[0]);
-		else
-			execution(pathcommand, command);
+		if (cond == -1)
+		{
+			status = params->status;
+			_printf("BenShell($) \n");
+			free_params(params);
+			return (status);
+		}
+		for (i = 0; i < BUFFER_SIZE; i++)
+			(params->buffer)[i] = 0;
+		params->tokCount = 0;
+		if (isatty(STDIN_FILENO))
+			_printf("BenShell($): ");
+		/*cond = _getline(params);*/
+		cond = getline(&params->buffer, &size, stdin);
+		params->lineCount++;
+		if (cond == -1 && _strlen(params->buffer) == 0)
+		{
+			status = params->status;
+			free_params(params);
+			return (status);
+		}
+		state = NULL;
+		params->nextCommand = _strtok(params->buffer, ";\n", &state);
+		while (params->nextCommand)
+		{
+			params->tokCount = process_string(params);
+			if (params->tokCount == 0)
+				break;
+			run_command(params);
+			for (i = 0; i < params->argsCap; i++)
+			{
+				free(params->args[i]);
+				params->args[i] = NULL;
+			}
+			params->tokCount = 0;
+			free(params->nextCommand);
+			params->nextCommand = _strtok(params->buffer, ";\n",
+						      &state);
+		}
 	}
-	if (linesize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(line);
-	return (0);
+}
+/**
+ * init_param - initialize params
+ * @argv: command line argument
+ * @env: environment variables
+ * Return: param on success
+ */
+param_t *init_param(char **argv, char **env)
+{
+	unsigned int i;
+	char *eqs = NULL;
+	param_t *params = malloc(sizeof(*params));
+
+	if (!params)
+		return (NULL);
+	params->argsCap = 10;
+	params->lineCount = 0;
+	params->tokCount = 0;
+	params->status = 0;
+	params->argv = argv;
+	params->nextCommand = NULL;
+	params->buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	if (!(params->buffer))
+	{
+		free(params);
+		exit(-1);
+	}
+	for (i = 0; i < BUFFER_SIZE; i++)
+		params->buffer[i] = 0;
+	params->args = malloc(sizeof(char *) * params->argsCap);
+	if (!(params->args))
+	{
+		free(params->buffer);
+		free(params);
+		exit(-1);
+	}
+	for (i = 0; i < params->argsCap; i++)
+		params->args[i] = NULL;
+	params->env_head = NULL;
+	for (i = 0; env[i]; i++)
+	{
+		eqs = _strchr(env[i], '=');
+		*eqs = '\0';
+		params->env_head = add_node(&(params->env_head),
+					    env[i], eqs + 1);
+		if (!(params->env_head))
+		{
+			free(params->buffer);
+			free(params->args);
+			free_list(params->env_head);
+			free(params);
+			exit(-1);
+		}
+	}
+	params->alias_head = NULL;
+	return (params);
 }
--- shell.h
+++ shell.h
@@ -1,62 +1,82 @@
-
-#ifndef SHELL_H
-#define SHELL_H
-
-#include <stdarg.h>
+#ifndef _SHELL_H
+#define _SHELL_H
+#define _GNU_SOURCE
+#include <stdio.h>
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <stdbool.h>
-
-/* environment variables */
-extern char **environ;
-extern __sighandler_t signal(int __sig, __sighandler_t __handler);
-
-/* handle built ins */
-int checker(char **cmd, char *buf);
-void prompt_user(void);
-void handle_signal(int m);
-char **tokenizer(char *line);
-char *test_path(char **path, char *command);
-char *append_path(char *path, char *command);
-int handle_builtin(char **command, char *line);
-void exit_cmd(char **command, char *line);
-
-void print_env(void);
-
-/* string handlers */
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-int _strncmp(char *s1, char *s2, int n);
-char *_strdup(char *s);
-char *_strchr(char *s, char c);
-
-void execution(char *cp, char **cmd);
-char *find_path(void);
-
-/* helper function for efficient free */
-void free_buffers(char **buf);
-
-struct builtin
+/**
+ * struct param_s - structure used to hold all shell variables needed
+ * @argv: command line argument from main function
+ 
+ * @buffer: input buffer
+ 
+ * @args: array of arguments
+ * @lineCount: total line of input
+ * @nextCommand: the next command to process
+ * @argsCap: num of arguments the args array can hold
+ * @lineCount: total line of input
+ * @tokCount: num of tokens in a line input
+ * @status: run command return status
+ * @env_head: singly linked list of environment vars
+ * @alias_head: singly linked list of aliases
+ * Description: This structures hold all variables that passed into
+ * other functions.
+ */
+typedef struct param_s
 {
-	char *env;
-	char *exit;
-} builtin;
+	char **argv;
+	char *buffer;
+	char **args;
+	char *nextCommand;
+	unsigned int argsCap;
+	unsigned int lineCount;
+	unsigned int tokCount;
+	int status;
+	list_t *env_head;
+	list_t *alias_head;
+} param_t;
 
-struct info
+/**
+ * struct op - a builtin function
+ * @name: builtin name
+ * @func: pointer to builtin function
+ */
+typedef struct op
 {
-	int final_exit;
-	int ln_count;
-} info;
+	char *name;
+	void (*func)(param_t *);
+} op_t;
 
-struct flags
-{
-	bool interactive;
-} flags;
-
-#endif /* SHELL_H */
+int process_string(param_t *);
+char *_strdup(char *);
+void _alias(param_t *params);
+char *_strcpy(char *dest, const char *src);
+int _getline(param_t *);
+void set_alias(char *name, param_t *params);
+void make_alias(char *name, char *val, param_t *params);
+void print(char *name, param_t *params);
+void print_all_aliases(param_t *params);
+void print_list_env(list_t *);
+void print_list_alias(list_t *);
+void free_params(param_t *params);
+void write_error(param_t *params, char *msg);
+int _strcmp(char *, char *);
+int _strcmp_n(char *, char *, int n);
+char *_strtok(char *str, char *delim, char **savePtr);
+void run_command(param_t *);
+void *_realloc(char **ptr, unsigned int old_size, unsigned int new_size);
+void _printenv(param_t *);
+void sigint_handler(int);
+char *_getenv(char *name, param_t *params);
+void _setenv(param_t *params);
+void _unsetenv(param_t *params);
+char *get_file(param_t *params);
+void (*get_builtin(param_t *params))(param_t *);
+void _myExit(param_t *params);
+void _cd(param_t *params);
+void _clear(param_t *params);
+#endif
